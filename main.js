8f9afe7fd0187d70c38d7979386bc714a5c1d8a7
charvi1k <85107234+charvi1k@users.noreply.github.com>
2021-12-13 16:27:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-function adduser()
-{
-   player_1name = document.getElementById("name_1input").Value;
-   player_2name = document.getElementById("name_2input").Value;
+player1_name = localStorage.getItem("input_no1");
+player2_name = localStorage.getItem("input_no2");
 
-   localStorage.setItem("player_1name", player_1name);
-   localStorage.setItem("player_2name", player_2name);
+player1_score = 0;
+player2_score = 0;
 
-   window.location = "game_page.html";
-}
+document.getElementById("input_no1").innerHTML = input_no1 + "=";
+document.getElementById("input_no2").innerHTML = input_no2 + "=";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question turn = " + input_no1;
+document.getElementById("player_answer").innerHTML = "Answer turn = " + input_no2;
+
